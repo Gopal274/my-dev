15ee01a3b7d3bf08f192b7a0977d2551c8e961a3
Gopal274 <gopaljha0877@gmail.com>
2025-05-27 08:02:50 +0530

    update javascript/basics/app.js

--- javascript/basics/app.js
+++ javascript/basics/app.js
@@ -1,7 +1,13 @@
-alert("ghost is comming")
-console.warn("this is my first warning")
-console.log({name:"gopal",age:17})  
-console.table({name:"gopal ",age:17})
-console.break("help")
 
-
+console.table({name:"gopal", age:10})
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.clear
+console.log("hello")
+console.log({name:"alex",
+  age:27,
+})
