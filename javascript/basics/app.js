ee4447ff57e0f3f50924c3088f5ee748c5b943f5
Gopal274 <gopaljha0877@gmail.com>
2025-05-28 16:34:29 +0530

    update javascript/basics/app.js

--- javascript/basics/app.js
+++ javascript/basics/app.js
@@ -256,10 +256,10 @@ let money = 50;
 // - Convert string to number
 //money = parseInt(money);
 //money = +money
+//money = Number(money);
 
 // - Convert number to string 
 money = money.toString();
-//money = Number(money);
 console.log(money);
 console.log(typeof money);
 
