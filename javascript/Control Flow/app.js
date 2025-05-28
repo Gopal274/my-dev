c8ee2d10cbe52e748da0af7632b9abbabe0b6d8c
Gopal274 <gopaljha0877@gmail.com>
2025-05-28 21:29:45 +0530

    hello

--- javascript/Control Flow/app.js
+++ javascript/Control Flow/app.js
@@ -95,4 +95,20 @@ switch (day) {
 //5. default "I have never heard of that fruit"
 
 
+let fruit = "banana";
 
+switch (fruit) {
+    case "banana":
+        console.log("Banana is good!");
+        break;
+    case "orange":
+        console.log("I am not a fan of orange.");
+        break;
+    case "apple":
+        console.log("Hope you like them apples?");
+        break;
+    default:
+        console.log("I have never heard of that fruit");
+
+
+}
