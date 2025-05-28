03b34d685ac8ecdd5b3b3747020d210ab7e0f51e
Gopal274 <gopaljha0877@gmail.com>
2025-05-28 17:33:46 +0530

    update javascript/Control Flow/app.js

--- javascript/Control Flow/app.js
+++ javascript/Control Flow/app.js
@@ -5,7 +5,7 @@ if (a > b) {
     console.log("a is greater than b");
 } else if (a < b) {
     console.log("a is less then b");
+} else {
+    console.log("a is equal to b");
 }
-else {
-  console.log("a is equal to b")
-}
+
