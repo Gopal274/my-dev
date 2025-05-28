c2f3081acf0668b003ad494635c7b2c7461399b1
Gopal274 <gopaljha0877@gmail.com>
2025-05-28 08:15:35 +0530

    update javascript/basics/app.js

--- javascript/basics/app.js
+++ javascript/basics/app.js
@@ -233,5 +233,5 @@ let secondName = "arena";
 // console.log(fullName);
 
 let desc = "this is some random";
-let fullName =`${firstName} ${secondName};
-console.log(fullName);
+let fullName = `${firstName} ${secondName}`;
+console.log(fullName)
