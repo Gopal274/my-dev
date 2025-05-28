d4046d36f822778ea0d450bb537a722eb183c27d
Gopal274 <gopaljha0877@gmail.com>
2025-05-28 17:32:39 +0530

    update javascript/Control Flow/app.js

--- javascript/Control Flow/app.js
+++ javascript/Control Flow/app.js
@@ -1,9 +1,8 @@
-let a = 10 
-let b = 20 
+let a = 10;
+let b = 20;
 
-if ( a > b){
-  console.log("a is greater than b")
-}
-else if (a < b) {
-  console.log("a is less then b")
+if (a > b) {
+    console.log("a is greater than b");
+} else if (a < b) {
+    console.log("a is less then b");
 }
