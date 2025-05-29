92ecaf56e45bb26596d647302e2bde54c61e6be1
Gopal274 <gopaljha0877@gmail.com>
2025-05-29 06:54:29 +0530

    update javascript/Control Flow/app.js

--- javascript/Control Flow/app.js
+++ javascript/Control Flow/app.js
@@ -113,6 +113,10 @@ switch (fruit) {
 // for loop -->
 
 for (let i = 0; i <= 5 ; i++ ){
-  console.log("hello world" , i)
+  console.log('---outer loop---' , i)
+  // nested loop 
+  for (let j = 0 ; j <= 5 ; j++ ){
+    console.log('---inner loop ---' j)
+  }
 }
 
